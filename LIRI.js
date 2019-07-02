07c63f20a467773d7658f4212877562f6230ff7b
dudekdawid89 <dudekdawid89@gmail.com>
2019-07-01 22:05:14 -0500

    spotify function set up

--- LIRI.js
+++ LIRI.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
-var axios = require("axios");
-var moment = require("moment");
-var fs = require("fs");
+// var axios = require("axios");
+// var moment = require("moment");
+// var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
 var userOption = process.argv[2];
@@ -28,3 +28,30 @@ function userInput(userOption, userInput){
             console.log("Wrong Option. Please use this options to proceed: concert-this, spotify-this-song, movie-this, do-what-it-says");
     }
 }
+
+var concertData = function(userInput){
+    if(userInput === undefined){
+        userInput = "The Sign"
+    }
+    spotify.search({
+        type: "track",
+        query: userInput
+    },
+    function(err,data){
+        if(err){
+            console.log("Error occured:" +err);
+            return;
+        }
+        var tracks = data.tracks.items;
+
+        for( var i=0; i< tracks.length; i++){
+            console.log(i);
+            console.log("artist(s):" + tracks[i].artists[0].name);
+            console.log("The song's name" +tracks[i].name);
+            console.log("A preview link of the song:"+ tracks[i].preview_url);
+            console.log("Songs:" +tracks[i].album.name);
+            
+        }
+    }
+    );
+};
